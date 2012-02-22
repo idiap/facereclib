366fce2e768e384f1cd6e22b1f3b32d0c972efeb
Manuel Guenther <manuel.guenther@idiap.ch>
2012-02-22 11:15:58 +0100

    added some documentation

--- script/parameter_tests.py
+++ script/parameter_tests.py
@@ -10,8 +10,11 @@ import utils
 from docutils.readers.python.pynodes import parameter
 
 
+# The different steps of the preprocessing chain.
+# Use these keywords to change parameters of the specific part
 steps = ['preprocessing', 'features', 'projection', 'enrol', 'scores']
 
+# Parts that could be skipped when the dependecies are on the indexed level
 skips = [[''], 
          ['--skip-preprocessing'], 
          ['--skip-feature-extraction-training', '--skip-feature-extraction'],
@@ -19,9 +22,12 @@ skips = [[''],
          ['--skip-enroler-training', '--skip-model-enrolment']
         ]
 
+# The keywords to parse the job ids to get the according dependencies right 
 dkeys  = ['DUMMY', 'preprocessing', 'feature_extraction', 'feature_projection', 'enrol']
 
+
 def next_level(config, index):
+  """Searches the config for the next non-empty level, i.e., where the according keyword is defined in the configuration"""
   for i in range(index, len(steps)):
     if hasattr(config, steps[i]):
       return getattr(config, steps[i])
@@ -29,6 +35,8 @@ def next_level(config, index):
 
 
 def write_config_file(args, infile_name, sub_dir, keyword, value):
+  """Copies the given configuration file by replacing the lines including the given keyword with the given value.
+     The function returnes the name of the newly generated file."""
   # read the config file
   infile = open(infile_name, 'r')
   outfile_name = os.path.join(args.config_dir, sub_dir, os.path.basename(infile_name))
@@ -53,6 +61,8 @@ def write_config_file(args, infile_name, sub_dir, keyword, value):
 
 
 def directory_parameters(dirs):
+  """This function generates the faceverify parameters that define the directories, where the data is stored. 
+     The directories are set such that data is reused whenever possible, but disjoined if needed."""
   parameters = []  
   # add directory parameters
   if dirs['preprocessing'] != '':
@@ -67,7 +77,9 @@ def directory_parameters(dirs):
     parameters.extend(['--score-sub-dir', dirs['scores']])
   return parameters 
 
+
 def get_deps(job_ids, index):
+  """Returns the dependencies for the given level from the given job ids""" 
   # get the dependencies
   deps = []
   for k in sorted(job_ids.keys()):
@@ -80,22 +92,24 @@ def get_deps(job_ids, index):
   
 
 def get_skips(index):
+  """Returns the skip parameters for the given level"""
   # get the skip parameters
   the_skips = []
   for i in range(1,index+1):
     the_skips.extend(skips[i])
-  print "Skips for step '%s' are: "%steps[index], the_skips
+#  print "Skips for step '%s' are: "%steps[index], the_skips
   return the_skips
 
 
 def execute_dependent_task(args, feature_file, tool_file, dirs, skips, deps):
+  """Executes the face verification task using the given feature and tool configurations, setting dependencies to the given dependent jobs"""
   # invoke face verification with the new configuration, including proper dependencies
   parameters = args.parameters[1:]
   parameters.extend(['-p', feature_file, '-t', tool_file])
   parameters.extend(directory_parameters(dirs))
   parameters.extend(skips)
   
-  print parameters
+#  print parameters
   verif_args = verif.parse_args(parameters)
   
 #  return []
@@ -105,6 +119,7 @@ def execute_dependent_task(args, feature_file, tool_file, dirs, skips, deps):
 
 
 def remove_keyword(keyword, config):
+  """This function removes the given keyword from the given configuration (and returns a copy of it)"""
   new_config = {}
   for key in config.keys():
     if key != keyword:
@@ -112,20 +127,8 @@ def remove_keyword(keyword, config):
   return new_config
 
 
-def default_dir(config, sub_dir = None):
-  dir = sub_dir
-  for keyword in config.keys():
-    for sub_dir in config[keyword]:
-      if config[keyword][sub_dir] == None:
-        if dir == None:
-          dir = sub_dir
-        else:
-          dir = os.path.join(dir, sub_dir)
-  return dir
-    
 global job_ids
 job_ids = {}
-first = {}
 
 def execute_recursively(args, config, index, current_setup, dirs, feature_file, tool_file, dependency_level):
   if current_setup == None:
@@ -149,7 +152,7 @@ def execute_recursively(args, config, index, current_setup, dirs, feature_file, 
 #      print "integrating job ids:", new_job_ids
 #      print "into old job ids:", job_ids
       job_ids.update(new_job_ids)
-#      print "result:", job_ids
+      print "The registered job ids are now:", job_ids
         
   else:
 #    print "\nEntering step", steps[index], "for recursive calls"
@@ -161,19 +164,22 @@ def execute_recursively(args, config, index, current_setup, dirs, feature_file, 
     
 #    print remaining_setup
     
-    first[steps[index]] = True
+    # The first job is dependent on the given dependency level, 
+    # while the following jobs are dependent on this level only 
+    first = True
     # iterate through the replacements
     for sub in replacements.keys():
       dirs[steps[index]] = os.path.join(dirs[steps[index-1]], sub)
       # replace the current keyword with the current replacement
       new_feature_file = write_config_file(args, feature_file, dirs[steps[index]], keyword, replacements[sub]) if index < 2 else feature_file
       new_tool_file = write_config_file(args, tool_file, dirs[steps[index]], keyword, replacements[sub]) if index >= 2 else tool_file
-      execute_recursively(args, config, index, remaining_setup, dirs, new_feature_file, new_tool_file, dependency_level if first[steps[index]] else index)
-      first[steps[index]] = False
+      execute_recursively(args, config, index, remaining_setup, dirs, new_feature_file, new_tool_file, dependency_level if first else index)
+      first = False
     
 #  print "Leaving step", steps[index], "\n"
 
 def main():
+  """Main entry point for the parameter test. Try --help to see the parameters that can be specified."""
   # set up command line parser
   parser = argparse.ArgumentParser(description=__doc__,
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -189,8 +195,11 @@ def main():
   parser.add_argument('-C', '--config-dir', type = str, dest='config_dir', default = '.',
                       help = 'Directory where the automatically generated config files should be written into')
   
-  parser.add_argument('parameters', nargs = argparse.REMAINDER)
+  # These are the parameters that are forwarded to the face verify script. Use -- to separate the para 
+  parser.add_argument('parameters', nargs = argparse.REMAINDER,
+                      help = 'Parameters directly passed to the face verify script. It should at least include the -d (and the -g) option. Use -- to separate this parameters from the parameters of this script.')
   
+  # parse arguments 
   args = parser.parse_args()
 
 
@@ -208,7 +217,7 @@ def main():
     # copy directory from previous index
     if hasattr(config, steps[i]):
       execute_recursively(args, config, i, next_level(config,i), dirs, args.features, args.tool, 0)
-      break
+      return
   
 
 if __name__ == "__main__":
