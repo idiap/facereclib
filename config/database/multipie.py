7bb0525e5afbe4f4bcb70900f381e8f5143b6e7d
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2012-02-29 18:28:08 +0100

    Add Multi-PIE support

---
+++ config/database/multipie.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import bob
+
+# setup for Multi-PIE database
+name = 'multipie'
+db = bob.db.multipie.Database()
+protocol = 'U'
+
+img_input_dir = "/idiap/resource/database/Multi-Pie/data"
+img_input_ext = ".png"
+pos_input_dir = "/idiap/user/rwallace/work/databases/multipie/annotations"
+pos_input_ext = ".pos"
+
+first_annot = 1
+all_files_options = { 'expressions': 'neutral' }
+world_extractor_options = {}
+world_projector_options = { 'expressions': 'neutral', 'world_sampling': 3, 'world_first': True } 
+world_enroler_options = { 'expressions': 'neutral' }
+
