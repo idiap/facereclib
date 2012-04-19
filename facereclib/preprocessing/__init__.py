00743cbbf21073edb1e541b3035a1e1d8f19ed0f
Manuel Guenther <manuel.guenther@idiap.ch>
2012-04-19 11:17:21 +0200

    Added missing preprocessing modifications.

--- facereclib/preprocessing/__init__.py
+++ facereclib/preprocessing/__init__.py
@@ -14,12 +14,12 @@ class NullPreprocessor:
   def __init__(self, config):
     pass
     
-  def __call__(self, input_file, eye_pos):
+  def __call__(self, input_file, eye_pos, output_file):
     image = bob.io.load(str(input_file))
     # convert to grayscale
     if(image.ndim == 3):
       image = bob.ip.rgb_to_gray(image)
-    return image
+    bob.io.save(image, output_file)
 
 
 
@@ -32,7 +32,7 @@ class FaceCrop:
     self.m_fen = bob.ip.FaceEyesNorm(config.CROP_EYES_D, config.CROP_H, config.CROP_W, config.CROP_OH, config.CROP_OW)
     self.m_fen_image = numpy.ndarray((config.CROP_H, config.CROP_W), numpy.float64) 
 
-  def __call__(self, input_file, eye_pos):
+  def __call__(self, input_file, eye_pos, output_file):
     """Reads the input image, normalizes it according to the eye positions, and writes the resulting image"""
     image = bob.io.load(str(input_file))
     # convert to grayscale
@@ -42,8 +42,8 @@ class FaceCrop:
     # perform image normalization
     self.m_fen(image, self.m_fen_image, eye_pos[1], eye_pos[0], eye_pos[3], eye_pos[2])
 
-    # return the normalized image    
-    return self.m_fen_image
+    # save output image    
+    bob.io.save(self.m_fen_image, output_file)
     
     
     
@@ -58,7 +58,7 @@ class TanTriggs:
     self.m_tan = bob.ip.TanTriggs(config.GAMMA, config.SIGMA0, config.SIGMA1, config.SIZE, config.THRESHOLD, config.ALPHA)
     self.m_tan_image = numpy.ndarray((config.CROP_H, config.CROP_W), numpy.float64)
 
-  def __call__(self, input_file, eye_pos):
+  def __call__(self, input_file, eye_pos, output_file):
     """Reads the input image, normalizes it according to the eye positions, and writes the resulting image"""
     image = bob.io.load(str(input_file))
     # convert to grayscale
@@ -71,8 +71,8 @@ class TanTriggs:
     # perform Tan-Triggs
     self.m_tan(self.m_fen_image, self.m_tan_image)
     
-    # return the normalized image
-    return self.m_tan_image
+    # save output image    
+    bob.io.save(self.m_tan_image, output_file)
     
     
 
