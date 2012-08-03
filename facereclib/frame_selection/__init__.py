8357a411734f2f8b01ddc66e74e27418c0ad0c24
Roy Wallace <roy.wallace@idiap.ch>
2012-08-03 17:00:20 +0200

    V2V: add a couple of debug print statements

--- facereclib/frame_selection/__init__.py
+++ facereclib/frame_selection/__init__.py
@@ -17,6 +17,7 @@ class AllFrameSelector:
     """Yields all frames of the specified VideoFrameContainer,
     sorted by ascending frame_id."""
     for frame in sorted(frame_container.frames(), key=lambda x: x[0]):
+      print "--> AllFrameSelector selects frame " + str(frame[0]) + ", quality " + str(frame[2][0]) # TODO: remove debug
       yield frame[1]
 
 class FirstNFrameSelector:
@@ -29,6 +30,7 @@ class FirstNFrameSelector:
     """Yields the first N frames of the specified VideoFrameContainer. The video must contain at least N frames, otherwise the behaviour is unspecified."""
     sorted_frames = sorted(frame_container.frames(), key=lambda x: x[0])
     for n in range(self._N):
+      print "--> FirstNFrameSelector selects frame " + str(sorted_frames[n][0]) + ", quality " + str(sorted_frames[n][2][0]) # TODO: remove debug
       yield sorted_frames[n][1]
 
 class QualityNFrameSelector:
@@ -42,4 +44,5 @@ class QualityNFrameSelector:
     """Yields the N frames with highest value in the k'th field of their corresponding quality vectors (k>=0). The VideoFrameContainer must contain at least N frames, otherwise the behaviour is unspecified."""
     sorted_frames = sorted(frame_container.frames(), key=lambda x: x[2][self._k], reverse=True)
     for n in range(self._N):
+      print "--> QualityNFrameSelector selects frame " + str(sorted_frames[n][0]) + ", quality " + str(sorted_frames[n][2][k]) # TODO: remove debug
       yield sorted_frames[n][1]
