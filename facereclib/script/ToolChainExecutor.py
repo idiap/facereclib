2a3dcc754a61ed576f6574bc6780bb8826d5496f
Manuel Guenther <manuel.guenther@idiap.ch>
2012-08-21 13:21:24 +0200

    Whitespace corrections.

--- facereclib/script/ToolChainExecutor.py
+++ facereclib/script/ToolChainExecutor.py
@@ -12,10 +12,10 @@ from .. import toolchain
 
 
 class ToolChainExecutor:
-  """This class is a helper class to provide functionality to execute 
-  tool chains. It manages the configuration files and the command line 
-  options, as well as the grid execution of the tasks.""" 
-  
+  """This class is a helper class to provide functionality to execute
+  tool chains. It manages the configuration files and the command line
+  options, as well as the grid execution of the tasks."""
+
   def __init__(self, args):
     """Initializes the Tool chain executor"""
     # remember command line arguments
@@ -27,32 +27,32 @@ class ToolChainExecutor:
     self.m_feature_extractor_config = imp.load_source('feature_extractor', args.features)
     if args.grid:
       self.m_grid_config = imp.load_source('grid', args.grid)
-    
+
     self.__generate_configuration__()
-    
+
     # generate the tools that we will need
     self.m_preprocessor = self.m_preprocessor_config.preprocessor(self.m_preprocessor_config)
     self.m_feature_extractor = self.m_feature_extractor_config.feature_extractor(self.m_feature_extractor_config)
     self.m_tool = self.m_tool_config.tool(self.m_tool_config)
-    
+
   def required_command_line_options(parser):
     """Initializes the minimum command line options that are
     required to run this experiment"""
-    
+
     #######################################################################################
     ############## options that are required to be specified #######################
     config_group = parser.add_argument_group('\nConfiguration files that need to be specified on the command line')
-    config_group.add_argument('-d', '--database', metavar = 'FILE', type = str, required = True, 
+    config_group.add_argument('-d', '--database', metavar = 'FILE', type = str, required = True,
         help = 'The database configuration file')
-    config_group.add_argument('-t', '--tool-chain', type = str, dest = 'tool', required = True, metavar = 'FILE', 
+    config_group.add_argument('-t', '--tool-chain', type = str, dest = 'tool', required = True, metavar = 'FILE',
         help = 'The tool chain configuration file')
     config_group.add_argument('-p', '--preprocessing', metavar = 'FILE', type = str, dest = 'preprocessor', required = True,
         help = 'Configuration script for image preprocessing')
     config_group.add_argument('-f', '--features', metavar = 'FILE', type = str, required = True,
         help = 'Configuration script for extracting the features')
-    config_group.add_argument('-g', '--grid', metavar = 'FILE', type = str, 
+    config_group.add_argument('-g', '--grid', metavar = 'FILE', type = str,
         help = 'Configuration file for the grid setup; if not specified, the commands are executed on the local machine')
-    
+
     #######################################################################################
     ############## options to modify default directories or file names ####################
     dir_group = parser.add_argument_group('\nDirectories that can be changed according to your requirements')
@@ -74,7 +74,7 @@ class ToolChainExecutor:
         help = 'Name of the file to write the model enroler into')
     file_group.add_argument('-G', '--submit-db-file', type = str, metavar = 'FILE', default = 'submitted.db', dest = 'gridtk_db',
         help = 'The db file in which the submitted jobs will be written (only valid with the --grid option)')
-    
+
     sub_dir_group = parser.add_argument_group('\nSubdirectories of certain parts of the toolchain. You can specify directories in case you want to reuse parts of the experiments (e.g. extracted features) in other experiments. Please note that these directories are relative to the --temp-dir')
     sub_dir_group.add_argument('--preprocessed-image-directory', type = str, metavar = 'DIR', default = 'preprocessed', dest = 'preprocessed_dir',
         help = 'Name of the directory of the preprocessed images')
@@ -82,7 +82,7 @@ class ToolChainExecutor:
         help = 'Name of the directory of the features')
     sub_dir_group.add_argument('--projected-directory', type = str, metavar = 'DIR', default = 'projected', dest = 'projected_dir',
         help = 'Name of the directory where the projected data should be stored')
-  
+
     other_group = parser.add_argument_group('\nFlags that change the behaviour of the experiment')
     other_group.add_argument('-q', '--dry-run', action='store_true', dest='dry_run',
         help = 'Only report the grid commands that will be executed, but do not execute them')
@@ -108,54 +108,54 @@ class ToolChainExecutor:
         help = 'Skip the score computation step')
     skip_group.add_argument('--skip-concatenation', '--nocat', action='store_true', dest='skip_concatenation',
         help = 'Skip the score concatenation step')
-                        
+
     return (config_group, dir_group, file_group, sub_dir_group, other_group, skip_group)
-  
+
   # make this methos static.
   required_command_line_options = staticmethod(required_command_line_options)
-     
+
 
   def set_common_parameters(self, calling_file, parameters, fake_job_id = 0):
     """Sets the parameters that the grid jobs require to be called.
     Just hand over all parameters of the faceverify script, and this function will do the rest.
-    Please call this function before submitting jobs to the grid 
+    Please call this function before submitting jobs to the grid
     using the submit_jobs_to_grid function"""
-              
+
     # we want to have the executable with the name of this file, which is laying in the bin directory
     self.m_common_parameters = ''
     for p in parameters:
       self.m_common_parameters += p + ' '
 
-    # job id used for the dry-run 
+    # job id used for the dry-run
     self.m_fake_job_id = fake_job_id
- 
- 
-    # define the dir from which the current executable was called  
+
+
+    # define the dir from which the current executable was called
     self.m_bin_dir = os.path.realpath(os.path.dirname(sys.argv[0]))
     self.m_executable = os.path.join(self.m_bin_dir, os.path.basename(calling_file))
     # generate job manager and set the temp dir
     self.m_job_manager = gridtk.manager.JobManager(statefile = self.m_args.gridtk_db)
     self.m_temp_dir = self.m_configuration.base_output_TEMP_dir
-         
-         
+
+
   def protocol_specific_configuration(self):
-    """Overload this function to set up configurations that are 
-    sepcific for your verification protocol""" 
+    """Overload this function to set up configurations that are
+    sepcific for your verification protocol"""
     raise NotImplementedError
 
   def __generate_configuration__(self):
-    """generates the configuration based on configuration files 
+    """generates the configuration based on configuration files
     and command line arguments"""
     # copy database configuration
     self.m_configuration = self.m_database_config
     # add command line based arguments
-    
+
     user_name = os.environ['USER']
     if self.m_args.user_dir:
       self.m_configuration.base_output_USER_dir = os.path.join(self.m_args.user_dir, self.m_args.sub_dir)
     else:
       self.m_configuration.base_output_USER_dir = os.path.join("/idiap/user", user_name, self.m_database_config.name, self.m_args.sub_dir)
-  
+
     if self.m_args.temp_dir:
       self.m_configuration.base_output_TEMP_dir = os.path.join(self.m_args.temp_dir, self.m_args.sub_dir)
     else:
@@ -165,17 +165,17 @@ class ToolChainExecutor:
         self.m_configuration.base_output_TEMP_dir = os.path.join("/idiap/temp", user_name, self.m_database_config.name, self.m_args.sub_dir)
 
     self.m_configuration.extractor_file = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.extractor_file)
-    self.m_configuration.projector_file = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.projector_file) 
-    self.m_configuration.enroler_file = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.enroler_file) 
+    self.m_configuration.projector_file = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.projector_file)
+    self.m_configuration.enroler_file = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.enroler_file)
 
-    self.m_configuration.preprocessed_dir = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.preprocessed_dir) 
+    self.m_configuration.preprocessed_dir = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.preprocessed_dir)
     self.m_configuration.features_dir = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.features_dir)
     self.m_configuration.projected_dir = os.path.join(self.m_configuration.base_output_TEMP_dir, self.m_args.projected_dir)
-      
-      
+
+
     # call configurations for the specific protocol
     self.protocol_specific_configuration()
-      
+
 
 ################################################################################################
 ########################## Functions concerning the grid setup #################################
@@ -183,21 +183,21 @@ class ToolChainExecutor:
 
 
   def __generate_job_array__(self, list_to_split, number_of_files_per_job):
-    """Generates an array for the list to be split and the number of files that one job should generate""" 
+    """Generates an array for the list to be split and the number of files that one job should generate"""
     n_jobs = int(math.ceil(len(list_to_split) / float(number_of_files_per_job)))
     return (1,n_jobs,1)
-    
-    
+
+
   def indices(self, list_to_split, number_of_files_per_job):
     """This function returns the first and last index for the files for the current job ID.
-       If no job id is set (e.g., because a sub-job is executed locally), it simply returns all indices""" 
+       If no job id is set (e.g., because a sub-job is executed locally), it simply returns all indices"""
     # test if the 'SEG_TASK_ID' environment is set
     sge_task_id = os.getenv('SGE_TASK_ID')
     if sge_task_id == None:
       # task id is not set, so this function is not called from a grid job
       # hence, we process the whole list
       return (0,len(list_to_split))
-    else: 
+    else:
       job_id = int(sge_task_id) - 1
       # compute number of files to be executed
       start = job_id * number_of_files_per_job
@@ -206,7 +206,7 @@ class ToolChainExecutor:
 
 
   def submit_grid_job(self, command, list_to_split = None, number_of_files_per_job = 1, dependencies=[], name = None, **kwargs):
-    """Submits a job to the grid""" 
+    """Submits a job to the grid"""
 
     # create the command to be executed
     cmd = [
@@ -215,32 +215,32 @@ class ToolChainExecutor:
             command,
             self.m_common_parameters
           ]
-  
+
     # if no job name is specified, create one
-    if name == None:        
+    if name == None:
       name = command.split(' ')[0].replace('--','')
       log_sub_dir = command.replace(' ','__').replace('--','')
     else:
       log_sub_dir = name
-    # generate log directory 
+    # generate log directory
     logdir = os.path.join(self.m_temp_dir, 'logs', log_sub_dir)
-  
-    # generate job array 
+
+    # generate job array
     if list_to_split != None:
       array = self.__generate_job_array__(list_to_split, number_of_files_per_job)
     else:
       array = (1,1,1)
-  
+
     # create the grid wrapper for the command
     use_cmd = ['-S', os.path.join(self.m_bin_dir, 'python')]
     use_cmd.extend(cmd)
-    
+
     # submit the job to the job mamager
     if not self.m_args.dry_run:
       job = self.m_job_manager.submit(use_cmd, deps=dependencies, cwd=True,
           stdout=logdir, stderr=logdir, name=name, array=array,
           **kwargs)
-  
+
       print 'submitted:', job
       return job.id()
     else:
@@ -248,5 +248,5 @@ class ToolChainExecutor:
       print 'would have submitted job with id ', self.m_fake_job_id, "as:"
       print ' '.join(use_cmd[2:]), "\nwith dependencies", dependencies
       return self.m_fake_job_id
-    
+
 
--- facereclib/utils/annotations.py
+++ facereclib/utils/annotations.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
-# @author: Manuel Guenther <Manuel.Guenther@idiap.ch> 
+# @author: Manuel Guenther <Manuel.Guenther@idiap.ch>
 # @date: Thu Jul 19 17:09:55 CEST 2012
 #
 # Copyright (C) 2011-2012 Idiap Research Institute, Martigny, Switzerland
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,16 +23,16 @@ def read_annotations(file_name, annotation_type):
   """This function reads the given file of annotations.
   It returns a dictionary with the keypoint name as key and the position (y,x) as value.
   Depending on the annotation type, different file formats are supported."""
-  
+
   if not file_name:
     return None
-  
+
   if not os.path.exists(file_name):
     raise IOError("The annotation file '%s' was not found"%file_name)
   f = open(file_name, 'r')
 
   annotations = {}
-  
+
   if str(annotation_type) == 'eyecenter':
     # only the eye positions are written, all are in the first row
     line = f.readline()
@@ -40,13 +40,13 @@ def read_annotations(file_name, annotation_type):
     assert len(positions) == 4
     annotations['reye'] = (int(positions[1]),int(positions[0]))
     annotations['leye'] = (int(positions[3]),int(positions[2]))
-    
+
   elif str(annotation_type) == 'multipie':
     # multiple lines, one header line, each line contains one position
     # use position names from:  /idiap/group/biometric/databases/multipie/README.txt
     # see also annotation examples in /idiap/group/biometric/databases/multipie/annotations_examples
-    
-    # Left and right is always being seen from the subjects perspective. 
+
+    # Left and right is always being seen from the subjects perspective.
     # the abbreviations are:
     # - 'l...', 'r...': left and right object (usually the eyes only)
     # - '...l', '...r': left and part of the object (usually the mouth)
@@ -54,7 +54,7 @@ def read_annotations(file_name, annotation_type):
     # - '...o', '...i': outer and inner label of the object (eyes and eyebrows)
     # example: 'reyei': label of the *i*nner corner of the *r*ight *eye*
     count = int(f.readline())
-    
+
     if count == 6:
       # profile annotations
       labels = ['eye', 'nose', 'mouth', 'lipt', 'lipb', 'chin']
@@ -69,7 +69,7 @@ def read_annotations(file_name, annotation_type):
       print "WARNING! Labels of file '%s' are incomplete"%file_name
     else:
       raise ValueError("The number %d of annotations in file '%s' is not handled."%(count, file_name))
-      
+
     for i in range(count):
       line = f.readline()
       positions = line.split()
@@ -91,9 +91,9 @@ def read_annotations(file_name, annotation_type):
       else:
         # enumerate all other annotations
         annotations['key%d'%(i-1)] = (int(positions[1]),int(positions[0]))
-  
+
   elif str(annotation_type) == 'named':
-    # multiple lines, no header line, each line contains annotation and position 
+    # multiple lines, no header line, each line contains annotation and position
     for line in f:
       positions = line.split()
       assert len(positions) == 3
@@ -109,15 +109,15 @@ def read_annotations(file_name, annotation_type):
     # skip the first 7 items: type, pos, id, bounding-box
     for i in range(7,len(parts),3):
       annotations[parts[i]] = (int(parts[i+2]),int(parts[i+1]))
-      
+
     # HACK! left and right eye positions are exchanged; change them
     if 'leye' in annotations and 'reye' in annotations:
       annotations['leye'], annotations['reye'] = annotations['reye'], annotations['leye']
-      
+
     # WARNING! The labels of the other annotations might also be exchanged between left and right
     # WARNING! These labels might be WRONG
-  
+
   if 'leye' in annotations and 'reye' in annotations and annotations['leye'][1] < annotations['reye'][1]:
-    print "Warning: the eye annotations in file '" + file_name + "' might be exchanged!" 
-  
+    print "Warning: the eye annotations in file '" + file_name + "' might be exchanged!"
+
   return annotations
