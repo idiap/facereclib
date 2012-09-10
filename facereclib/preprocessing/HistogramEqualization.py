8c8cb340ea00c4c04e0f5d5f7a3a84ec7e59ff09
Manuel Guenther <manuel.guenther@idiap.ch>
2012-09-10 14:00:28 +0200

    Moved histogram equalization to Bobs new implementation.

--- facereclib/preprocessing/HistogramEqualization.py
+++ facereclib/preprocessing/HistogramEqualization.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
-# @author: Manuel Guenther <Manuel.Guenther@idiap.ch> 
+# @author: Manuel Guenther <Manuel.Guenther@idiap.ch>
 # @date: Thu May 24 10:41:42 CEST 2012
 #
 # Copyright (C) 2011-2012 Idiap Research Institute, Martigny, Switzerland
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -33,38 +33,24 @@ class HistogramEqualization (FaceCrop):
 
 
   def equalize_histogram(self, image):
-    image = image.astype(numpy.uint8)
-    histogram = bob.ip.histogram(image, 255).astype(numpy.float64)
-    histogram /= float(image.size)
-  
-    # compute cumulative histogram density function  
-    cdf = [0.] * len(histogram)
-    for i in range(1,len(histogram)):
-      cdf[i] = cdf[i-1] + histogram[i]
-    
-    # normalize image
-    if self.m_histogram_image.shape != image.shape:
-      self.m_histogram_image = numpy.ndarray(image.shape, numpy.float64)
-    for y in range(image.shape[0]):
-      for x in range(image.shape[1]):
-        # Multiply with 255 to shift the normalized cdf values to pixel ranges 0..255
-        self.m_histogram_image[y,x] = cdf[image[y,x]] * 255.
-    
+    """Calls bob function to do the actual histogram equalization"""
+    bob.ip.histogram_equalization(image.astype(numpy.uint8), self.m_histogram_image)
+
     return self.m_histogram_image
-    
+
 
   def __call__(self, input_file, output_file, annotations = None):
     """Reads the input image, normalizes it according to the eye positions, performs histogram equalization, and writes the resulting image"""
     # crop the face using the base class method
     image = self.crop_face(input_file, annotations)
-    
+
     # perform histogram equalization
     histogram_image = self.equalize_histogram(image)
-    
+
     if annotations != None:
       # set the positions that were masked during face cropping to 0
       histogram_image[self.m_mask == False] = 0.
-      
+
     # save the image to file
     bob.io.save(histogram_image, output_file)
 
