979eb16431e6c30982d7fa503de049f1802865dd
Manuel Guenther <manuel.guenther@idiap.ch>
2013-07-03 18:38:58 +0200

    Updated BANCA database configurations to use the novel comprised annotations.

--- facereclib/configurations/databases/banca_twothirds.py
+++ facereclib/configurations/databases/banca_twothirds.py
@@ -8,8 +8,7 @@ database = facereclib.databases.DatabaseXBobZT(
     name = "banca",
     image_directory = "/idiap/group/biometric/databases/banca/english/images/images/",
     image_extension = ".ppm",
-    annotation_directory = "/idiap/group/biometric/annotations/banca/english/images/annotations/",
-    annotation_type = 'eyecenter',
+    has_internal_annotations = True,
     protocol = 'P',
     projector_training_options = { 'subworld': "twothirds" }
 )
