580ba675e6d2affc7a08c7ea4c0b13ff60e88896
Manuel Guenther <manuel.guenther@idiap.ch>
2013-10-01 17:27:00 +0200

    Fixed bug in FRGC configuration file.

--- facereclib/configurations/databases/frgc.py
+++ facereclib/configurations/databases/frgc.py
@@ -3,10 +3,12 @@
 import xbob.db.frgc
 import facereclib
 
+frgc_directory = "/idiap/resource/database/frgc/FRGC-2.0-dist"
+
 database = facereclib.databases.DatabaseXBob(
-    database = xbob.db.frgc.Database(),
+    database = xbob.db.frgc.Database(frgc_directory),
     name = "frgc",
-    original_directory = "/idiap/resource/database/frgc/FRGC-2.0-dist",
+    original_directory = frgc_directory,
     original_extension = ".jpg",
     has_internal_annotations = True,
     protocol = '2.0.1',
