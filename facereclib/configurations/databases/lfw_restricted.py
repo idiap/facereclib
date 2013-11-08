3615a21c4fccdf1b6a25b6be0af86e6da9fa0c31
Manuel Guenther <manuel.guenther@idiap.ch>
2013-11-08 14:52:07 +0100

    Corrected configuration of LFW database to be more specific.
    Conflicts:
    	facereclib/configurations/databases/lfw_restricted.py

---
+++ facereclib/configurations/databases/lfw_restricted.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import xbob.db.lfw
+import facereclib
+
+lfw_directory = "[YOUR_LFW_DIRECTORY]"
+
+database = facereclib.databases.DatabaseXBob(
+    database = xbob.db.lfw.Database(),
+    name = 'lfw',
+    original_directory = lfw_directory,
+    original_extension = ".jpg",
+    protocol = 'view1',
+
+    all_files_options = {'world_type' : 'restricted'},
+    extractor_training_options = {'world_type' : 'restricted'}, # 'subworld' : 'twofolds'
+    projector_training_options = {'world_type' : 'restricted'}, # 'subworld' : 'twofolds'
+    enroller_training_options =  {'world_type' : 'restricted'} # 'subworld' : 'twofolds'
+)
+
