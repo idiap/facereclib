e0738f80ecdd354b6d5647a8fc005658d3cf8aa7
Manuel Guenther <manuel.guenther@idiap.ch>
2013-11-28 16:01:19 +0100

    Increased version number after publishing new version 1.2.1.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='facereclib',
-    version='1.2.1a0',
+    version='1.2.2a0',
     description='Compare a variety of face recognition algorithms by running them on many image databases with default protocols.',
 
     url='https://github.com/bioidiap/facereclib',
