6d87131b5824addd4ddfb169fd198bd38afad15c
Manuel Guenther <manuel.guenther@idiap.ch>
2014-05-07 17:50:14 +0200

    Increased version to new patch version before uploading to pypi.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='facereclib',
-    version='1.2.2',
+    version='1.2.3',
     description='Compare a variety of face recognition algorithms by running them on many image databases with default protocols.',
 
     url='http://github.com/idiap/facereclib',
