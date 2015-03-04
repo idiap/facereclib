05889697f6fd55ee6505c3beddd77f9ea968bc1b
Manuel Guenther <manuel.guenther@idiap.ch>
2015-03-04 16:01:26 +0100

    Simplified AT&T test database interface

--- facereclib/tests/databases/atnt_fl/atnt_fl_database.py
+++ facereclib/tests/databases/atnt_fl/atnt_fl_database.py
@@ -2,29 +2,22 @@ import bob.db.verification.filelist
 import facereclib
 import os
 
-class TestDatabase (facereclib.databases.DatabaseFileList):
+database = facereclib.databases.DatabaseFileList(
 
-  def __init__(self):
+  database = bob.db.verification.filelist.Database(
+    base_dir = os.path.realpath(os.path.dirname(__file__)),
+    original_directory = facereclib.utils.tests.atnt_database_directory(),
+    original_extension = ".pgm",
+    dev_subdir = '.',
+    eval_subdir = '.',
+    world_filename = 'world.lst',
+    models_filename = 'models.lst',
+    probes_filename = 'probes.lst',
+    tnorm_filename = 'models.lst',
+    znorm_filename = 'probes.lst',
+    keep_read_lists_in_memory = True
+),
+  name = 'test_fl',
+  protocol = None
 
-    # call base class constructor with useful parameters
-    facereclib.databases.DatabaseFileList.__init__(
-        self,
-        database = bob.db.verification.filelist.Database(
-            base_dir = os.path.realpath(os.path.dirname(__file__)),
-            original_directory = facereclib.utils.tests.atnt_database_directory(),
-            original_extension = ".pgm",
-            dev_subdir = '.',
-            eval_subdir = '.',
-            world_filename = 'world.lst',
-            models_filename = 'models.lst',
-            probes_filename = 'probes.lst',
-            tnorm_filename = 'models.lst',
-            znorm_filename = 'probes.lst',
-            keep_read_lists_in_memory = True
-        ),
-        name = 'test_fl',
-        protocol = None
-    )
-
-database = TestDatabase()
-
+)
